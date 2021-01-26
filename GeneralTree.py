dd20bdbe43196b62ff268be6e688d7e9060f77e5
unknown <akshat.aksharma@gmail.com>
2021-01-26 14:35:46 +0530

    Added a level get funtion and printing was also improved

--- GeneralTree.py
+++ GeneralTree.py
@@ -4,22 +4,31 @@ class TreeNode:
         self.children = []
         self. parent = None
     def add_child(self,child):
+        self.child = child 
         child.parent = self
         self.children.append(child)
-
+    def get_level(self):
+        level = 0 
+        p = self.parent
+        while p :
+            p = p.parent
+            level += 1
+        return level 
     def print_tree(self):
+        print('  '*self.get_level() + '|--', end = '')
         print(self.data)
-        for child in self.children:
-            print(child.data)
+        if self.children:
+            for each in self.children:
+                each.print_tree()
         
 def run():
     root = TreeNode('Eletronics')
     laptop = TreeNode('Laptop')
     root.add_child(laptop)
+
     laptop.add_child(TreeNode('Mac'))
     laptop.add_child(TreeNode('Windows'))
     laptop.add_child(TreeNode('Linux'))
-
     tv = TreeNode('TV')
     root.add_child(tv)
     tv.add_child(TreeNode('LG'))
@@ -29,7 +38,7 @@ def run():
 
     #return root
 if __name__ == '__main__':
-    run
+    run()
     pass
     
     
